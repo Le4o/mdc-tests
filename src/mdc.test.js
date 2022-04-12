07e1bbe4d671f299882c158fed137eb7854f900a
Leonardo de Andrade <devleonardoandrade@gmail.com>
2022-04-11 23:39:25 -0300

    chore: use describe.each

--- src/mdc.test.js
+++ src/mdc.test.js
@@ -1,19 +1,14 @@
 const { mdc } = require(".");
 
 // First Property
-describe("when b > 0 and is a divider from a, then mdc(a,b) = b", () => {
-  test("when a is a pair number", () => {
-    let a = 6;
-    let b = 3;
-    let expectedValue = 3;
-
-    expect(mdc(a, b)).toBe(expectedValue);
-  });
-  test("when a is a odd number", () => {
-    let a = 9;
-    let b = 3;
-    let expectedValue = 3;
-
-    expect(mdc(a, b)).toBe(expectedValue);
-  });
-});
+describe.each([
+  [6, 3, 3],
+  [9, 3, 3],
+])(
+  "when b > 0 and is a divider from a, then mdc(a,b) = b",
+  (a, b, expected) => {
+    test(`given a=${a}, b=${b}`, () => {
+      expect(mdc(a, b)).toBe(expected);
+    });
+  }
+);
